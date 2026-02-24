c7ba5d9eb00ad912450d8d8c18600f6fcc202390
devfabricadeprojetos <dev.calcule@gmail.com>
2026-02-23 22:24:26 -0300

    Add files via upload

---
+++ Elias/config.py
@@ -0,0 +1,16 @@
+"""
+ _______  ___      _______  ______    ___   _______  __   __ 
+|       ||   |    |   _   ||    _ |  |   | |       ||  | |  |
+|       ||   |    |  |_|  ||   | ||  |   | |    ___||  |_|  |
+|       ||   |    |       ||   |_||_ |   | |   |___ |       |
+|      _||   |___ |       ||    __  ||   | |    ___||_     _|
+|     |_ |       ||   _   ||   |  | ||   | |   |      |   |  
+|_______||_______||__| |__||___|  |_||___| |___|      |___|  
+
+Software: AIS (Analise de Inadimplencia e Taxa Selic)
+Autor: ELIAS OLIVEIRA
+Versão: 0.0.0.1
+Ultimo Release: 18/02/2026
+"""
+
+DB_PATH = "C:/Users/sabado/Desktop/Elias/dados.db"
